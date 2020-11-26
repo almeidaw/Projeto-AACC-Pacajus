be9e01c78151277aced1c216747a3c45f28d58f3
Mateus Galdino <mateusgaldino290@gmail.com>
2020-11-26 14:04:43 -0300

    Finalização e correção do código do carrossel de fotos

--- home_carrossel_fotos.js
+++ home_carrossel_fotos.js
@@ -8,26 +8,30 @@ var slideIndexFoto = 1;
 showSlidesFoto(slideIndexFoto);
 
 function plusSlidesFoto(n) {
-  showSlidesFoto(slideIndex += n);
+  showSlidesFoto(slideIndexFoto += n);
 }
 
 function currentSlideFoto(n) {
-  showSlidesFoto(slideIndex = n);
+  showSlidesFoto(slideIndexFoto = n);
 }
 
 function showSlidesFoto(n) {
   var i;
   var slides = document.getElementsByClassName("mySlidesFoto");
   var dots = document.getElementsByClassName("dotFoto");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndexFoto-1].style.display = "inline";
+  dots[slideIndexFoto-1].className += " active";
 } 
 
